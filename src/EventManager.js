0ba54b14039df9f030d24928f27e6f4258d14514
Joe Mirmina <jmirmina@gmail.com>
2013-01-29 09:55:26 -0500

    Added an option to the removeEventSource method to keep existing events on the calendar

--- src/EventManager.js
+++ src/EventManager.js
@@ -191,15 +191,20 @@ function EventManager(options, _sources) {
 	}
 	
 
-	function removeEventSource(source) {
+	function removeEventSource(source, keepEvents) {
+		keepEvents = (typeof keepEvents === "undefined") ? false : keepEvents;
+
 		sources = $.grep(sources, function(src) {
 			return !isSourcesEqual(src, source);
 		});
-		// remove all client events from that source
-		cache = $.grep(cache, function(e) {
-			return !isSourcesEqual(e.source, source);
-		});
-		reportEvents(cache);
+
+		if (!keepEvents) {
+			// remove all client events from that source
+			cache = $.grep(cache, function(e) {
+				return !isSourcesEqual(e.source, source);
+			});
+			reportEvents(cache);
+		}
 	}
 	
 	
